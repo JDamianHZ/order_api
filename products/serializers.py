74314ce9cdab4417cdbd8475f396859635d8e572
Gael Flores <florogael@gmail.com>
2025-08-08 11:04:19 -0600

    Change to Porduct app
    Change the models and settings from Inventory app to Product app

---
+++ products/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+#Serializador para categorias
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+#Serializador para productos
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True, source='category')
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'sku', 'price', 'stock', 'category', 'category_id', 'supplier']
