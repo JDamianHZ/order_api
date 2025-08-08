21041bf1aade9908b72bda36e5ac78dca1ed24c6
Emmanuel Arevalo <emmanue.arevalo.trabajo@gmail.com>
2025-08-07 21:17:33 -0600

    Agrega modelos, serializers, views y urls para Category y Product

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets,permissions
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+#CRUD de categorias
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.AllowAny]
+
+
+
+#CRUD de productos
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]
+
+#Filtro por categoria
+def get_queryset(self):
+    queryset = Product.objects.all()
+    category_id = self.request.query_params.get('category')
+    if category_id:
+        queryset = queryset.filter(category_id=category_id)
+    return queryset
